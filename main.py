c83b88ea85db12342e644c0380a1afada0030c25
CimimUxMaio <juanbiabdon@gmail.com>
2022-12-10 20:14:08 -0300

    Fixed default logging not showing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import asyncio
 from dotenv import load_dotenv
 from src.bebot import Bebot
 
@@ -12,12 +11,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = Bebot(command_prefix=".", intents=intents)
 
-# Main program
-async def main():
-    TOKEN = os.environ["DISCORD_TOKEN"]
-    async with bot:
-        await bot.start(TOKEN, reconnect=True)
-
 # Run program
 if __name__ == "__main__":
-    asyncio.run(main())
+    TOKEN = os.environ["DISCORD_TOKEN"]
+    bot.run(TOKEN)
