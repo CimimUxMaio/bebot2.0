9e3d7de80026f171cdbab286f7a280996457007b
CimimUxMaio <juanbiabdon@gmail.com>
2023-01-29 17:32:06 -0300

    Finish basic commands in music cog

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -13,7 +13,7 @@ class MusicCog(Cog, name="Music"):
     def __init__(self, bot: Bebot):
         self.bot = bot
 
-    @command(aliases=["p"], name="play")
+    @command(aliases=["p"], name="play", help="Queue the given song.")
     async def play(self, ctx: Context, *, search: str):
         songs = music_service.search_songs(search)
         music_client = self.get_music_client(ctx)
@@ -21,7 +21,24 @@ class MusicCog(Cog, name="Music"):
         await music_client.connect(voice_channel)
         await music_client.queue_songs(songs)
 
+    @command(aliases=["sk"], name="skip", help="Skips the current song.")
+    async def skip(self, ctx: Context):
+        self.get_music_client(ctx).skip_current_song()
+
+    @command(aliases=["st"],
+             name="stop",
+             help="Stops / resumes the song that is currently playing.")
+    async def stop(self, ctx: Context):
+        self.get_music_client(ctx).toggle_pause_resume()
+
+    @command(aliases=["l"], name="leave", help="Disconnects Bebot from the voice channel.")
+    async def leave(self, ctx: Context):
+        await self.get_music_client(ctx).disconnect()
+
     @play.before_invoke
+    @skip.before_invoke
+    @stop.before_invoke
+    @leave.before_invoke
     async def check_voice_channel(self, ctx: Context):
         author = cast(Member, ctx.author)
         if not author.voice or not author.voice.channel:
