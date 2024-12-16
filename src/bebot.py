f124c52e369f3df14ee77abc1268142f0c1c1da4
CimimUxMaio <juanbiabdon@gmail.com>
2024-12-16 13:52:45 -0300

    fix: update error handling in Bebot class
    - Replaced DomainError with UserError in error handling
    - Moved error message embedding inside on_command_error method
    - Simplified error handling logic by removing error_message method

--- src/bebot.py
+++ src/bebot.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from discord import Color, Embed
 from discord.ext.commands import Bot, CommandError, Context
-from src.exceptions import DomainError
+from src.exceptions import UserError
 
 
 class Bebot(Bot):
@@ -18,18 +18,13 @@ class Bebot(Bot):
     async def on_ready(self):
         print(f"Bot running as {self.user}.")
 
-    async def on_command_error(self, ctx: Context, error):
-        message = self.error_message(error)
-
-        embed = Embed(color=Color.red())
-        embed.add_field(name="Error", value=message)
-
-        await ctx.send(embed=embed, delete_after=5)
-
-    def error_message(self, error: CommandError) -> str:
+    async def on_command_error(self, ctx: Context, error: CommandError):
         error = getattr(error, "original", error)
 
-        if not isinstance(error, DomainError):
+        if not isinstance(error, UserError):
             raise error
 
-        return error.message
+        embed = Embed(color=Color.red())
+        embed.add_field(name="Error", value=error.message)
+
+        await ctx.send(embed=embed, delete_after=5)
